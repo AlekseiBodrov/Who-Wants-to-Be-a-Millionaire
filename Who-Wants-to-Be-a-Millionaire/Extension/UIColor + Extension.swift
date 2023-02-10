a547e304d3d8b5fddd4b0976638d673983f15a12
Ildar <ildaridrisovich@gmail.com>
2023-02-10 23:11:40 +0400

    add fonts, extension. Complete RegistrationVC, RepeatVC, QuestionsLibrary, QuestionsModel, ScheduleVC

---
+++ Who-Wants-to-Be-a-Millionaire/Extension/UIColor + Extension.swift
@@ -0,0 +1,17 @@
+//
+//  UIColor + Extension.swift
+//  Who-Wants-to-Be-a-Millionaire
+//
+//  Created by иван Бирюков on 05.02.2023.
+//
+
+import UIKit
+
+extension UIColor {
+    static let specialGreen = UIColor(red: 0.37, green: 0.64, blue: 0.38, alpha: 1)
+    static let specialBlue = UIColor(red: 0.20, green: 0.38, blue: 0.51, alpha: 1)
+    static let specialLightBlue = UIColor(red: 0.58, green: 0.83, blue: 0.88, alpha: 1)
+    static let specialViolet = UIColor(red: 0.51, green: 0.37, blue: 0.73, alpha: 1)
+    static let specialGold = UIColor(red: 0.78, green: 0.70, blue: 0.43, alpha: 1)
+    static let specialRed = UIColor(red: 0.78, green: 0.51, blue: 0.43, alpha: 1)
+}
---
+++ Who-Wants-to-Be-a-Millionaire/RepeatViewController.swift
@@ -0,0 +1,115 @@
+//
+//  RepeatViewController.swift
+//  Who-Wants-to-Be-a-Millionaire
+//
+//  Created by Ildar Garifullin on 07/02/2023.
+//
+
+import UIKit
+
+final class RepeatViewController: UIViewController {
+
+    //MARK: - let/var
+    private let backgroundImage: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "Frame 1")
+        imageView.contentMode = .scaleAspectFit
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        
+        return imageView
+    }()
+    
+    private let gameIconImage: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "Image 1")
+        imageView.contentMode = .scaleAspectFit
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        
+        return imageView
+    }()
+    
+    private let infoLoseLabel: UILabel = {
+        let label = UILabel()
+        label.text = "You losed on {attempt} attempt"
+        label.textColor = .white
+        label.font = .robotoMedium24()
+        label.textAlignment = .center
+        label.translatesAutoresizingMaskIntoConstraints = false
+        
+        return label
+    }()
+    
+    private let loseLabel: UILabel = {
+        let label = UILabel()
+        label.text = "LOSE"
+        label.textColor = .specialRed
+        label.font = .syneRegular50()
+        label.textAlignment = .center
+        label.translatesAutoresizingMaskIntoConstraints = false
+        
+        return label
+    }()
+    
+    var stackView = UIStackView()
+    
+    private let playAgainButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.backgroundColor = .specialGreen
+        button.layer.cornerRadius = 20
+        button.setTitle("PLAY AGAIN", for: .normal)
+        button.titleLabel?.font = .robotoMedium36()
+        button.tintColor = .white
+        button.addTarget(self, action: #selector(playAgainButtonTapped), for: .touchUpInside)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        
+        return button
+    }()
+    
+    //MARK: - life cycle funcs
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        configure()
+        setConstraints()
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        backgroundImage.frame = view.bounds
+    }
+    
+    //MARK: - flow funcs
+    private func configure() {
+        view.addSubview(backgroundImage)
+        
+        stackView = UIStackView(
+            arrangedSubviews: [gameIconImage, infoLoseLabel, loseLabel],
+            axis: .vertical,
+            spacing: 10)
+        
+        view.addSubview(stackView)
+        
+        view.addSubview(playAgainButton)
+    }
+    
+    @objc private func playAgainButtonTapped() {
+        print("playAgainButtonTapped")
+    }
+    
+    private func setConstraints() {
+        
+        NSLayoutConstraint.activate([
+            stackView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            stackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20),
+            stackView.heightAnchor.constraint(equalToConstant: 300)
+        ])
+        
+        NSLayoutConstraint.activate([
+            playAgainButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 62),
+            playAgainButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -80),
+            playAgainButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -62),
+            playAgainButton.heightAnchor.constraint(equalToConstant: 100)
+        ])
+    }
+}
