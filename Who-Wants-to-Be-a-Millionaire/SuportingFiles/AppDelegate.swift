4a5f86eef06b42d6fe946a7a6b11863e620c484c
Ivan Biriukov <Grottt0@mail.ru>
2023-02-12 02:41:50 +0300

    Finish step of Debugging

--- Who-Wants-to-Be-a-Millionaire/SuportingFiles/AppDelegate.swift
+++ Who-Wants-to-Be-a-Millionaire/SuportingFiles/AppDelegate.swift
@@ -9,5 +9,18 @@ import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        
+        let window = UIWindow()
+        window.rootViewController = RegistrationViewController()
+        self.window = window
+        self.window?.makeKeyAndVisible()
+        
+        return true
+        
+    }
 }
 
